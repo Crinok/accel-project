5a51285ff6ba9fa989fd479e30c20305d7ebc8b6
Chris Okebata <crinokgraphics@gmail.com>
2017-12-02 23:29:43 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -128,5 +128,51 @@ bigCircle = new Ball(canvas.width/2, canvas.height/2, canvas.width/4, 'transpare
 // objArray[objArray.length] = new Ball(canvas.width, canvas.height/2, 20, "transparent");
 objArray[objArray.length] = new Ball(canvas.width, canvas.height/2, 20, "red");
 ball = new Ball(canvas.width/2, canvas.height/2, 20, "white");
-draw();
 
+var ax, ay;
+
+if (window.DeviceMotionEvent != undefined) {
+    window.ondevicemotion = function(e) {
+        ax = event.accelerationIncludingGravity.x * - 7;
+        ay = event.accelerationIncludingGravity.y * - 7;
+    }
+
+    draw();
+}
+
+function Ball(x, y, radius, ballColor) {
+    this.radius = radius;
+    this.dx = ax;
+    this.dy = ay;
+    // mass is that of a sphere, except the constants like PI and 4/3
+    // reason for sphere over circle is, well, we're looking at spheres from above, duh
+    this.mass = this.radius * this.radius * this.radius;
+    this.x = x;
+    this.y = y;
+    this.color = ballColor;
+    this.draw = function() {
+        ctx.beginPath();
+        ctx.arc(Math.round(this.x), Math.round(this.y), this.radius, 0, 2*Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+        ctx.closePath();
+    };
+    this.speed = function() {
+        // magnitude of velocity vector
+        return Math.sqrt(this.dx * this.dx + this.dy * this.dy);
+    };
+    this.angle = function() {
+        //angle of ball with the xy plane
+        return Math.atan2(this.dy, this.dx);
+    };
+    this.kineticEnergy = function () {
+    // only for masturbation purposes, not rly used for computation.
+        return (0.5 * this.mass * this.speed() * this.speed());
+    };
+    this.onGround = function() {
+        return (this.y + this.radius >= canvas.height)
+    }
+}
