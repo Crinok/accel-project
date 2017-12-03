04958677bf7875688a741cea3e3a8364aaaf5dcf
Chris Okebata <crinokgraphics@gmail.com>
2017-12-03 10:41:58 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ var objArray = [];
 var frameRate = 20;
 var acceleration = 7;
 var gravity = 0.97;
+var bounciness = 8;
 
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -94,17 +95,39 @@ function wallCollisionBall() {
 
 }
 
+function boundingBoxCheck(){
+    if (x < 20) {
+        x = 20;
+        vx = -vx / bounciness; 
+    }
+
+    if (y < 20) {
+        y = 20;
+        vy = -vy / bounciness; 
+    }
+
+    if (x > document.documentElement.clientWidth-20) {
+        x = document.documentElement.clientWidth-20;
+        vx = -vx / bounciness; 
+    }
+    if (y > document.documentElement.clientHeight-20) {
+        y = document.documentElement.clientHeight-20;
+        vy = -vy / bounciness; 
+    }   
+    
+}
+
 function ballCollision() {
     for (var obj1 in objArray) {
         for (var obj2 in objArray) {
-            if (obj1 !== obj2 && distanceNextFrame(objArray[obj1], objArray[obj2]) <= 0) {
+            if (ball !== obj2 && distanceNextFrame(ball, objArray[obj2]) <= 0) {
                 // ballCollisionSafety();
-                var theta1 = objArray[obj1].angle();
+                var theta1 = ball.angle();
                 var theta2 = objArray[obj2].angle();
-                var phi = Math.atan2(objArray[obj2].y - objArray[obj1].y, objArray[obj2].x - objArray[obj1].x);
-                var m1 = objArray[obj1].mass;
+                var phi = Math.atan2(objArray[obj2].y - ball.y, objArray[obj2].x - ball.x);
+                var m1 = ball.mass;
                 var m2 = objArray[obj2].mass;
-                var v1 = objArray[obj1].speed();
+                var v1 = ball.speed();
                 var v2 = objArray[obj2].speed();
 
                 var dx1F = (v1 * Math.cos(theta1 - phi) * (m1-m2) + 2*m2*v2*Math.cos(theta2 - phi)) / (m1+m2) * Math.cos(phi) + v1*Math.sin(theta1-phi) * Math.cos(phi+Math.PI/2);
@@ -112,8 +135,8 @@ function ballCollision() {
                 var dx2F = (v2 * Math.cos(theta2 - phi) * (m2-m1) + 2*m1*v1*Math.cos(theta1 - phi)) / (m1+m2) * Math.cos(phi) + v2*Math.sin(theta2-phi) * Math.cos(phi+Math.PI/2);
                 var dy2F = (v2 * Math.cos(theta2 - phi) * (m2-m1) + 2*m1*v1*Math.cos(theta1 - phi)) / (m1+m2) * Math.sin(phi) + v2*Math.sin(theta2-phi) * Math.sin(phi+Math.PI/2);
 
-                objArray[obj1].dx = dx1F;                
-                objArray[obj1].dy = dy1F;                
+                ball.dx = dx1F;                
+                ball.dy = dy1F;                
                 objArray[obj2].dx = dx2F;                
                 objArray[obj2].dy = dy2F;
             }
@@ -146,6 +169,7 @@ function drawObjects() {
 
 function draw() {
     clearCanvas();
+    boundingBoxCheck();
     ball = new Ball(x, y, 20, "white");
     drawObjects();
     // moveBall();
@@ -159,7 +183,7 @@ function draw() {
 
 bigCircle = new Ball(canvas.width/2, canvas.height/2, canvas.width/4, 'transparent');
 // objArray[objArray.length] = new Ball(canvas.width, canvas.height/2, 20, "transparent");
-objArray[objArray.length] = new Ball(canvas.width, canvas.height/2, 20, "red");
+objArray[objArray.length] = new Ball(canvas.width, canvas.height/2, 20, "green");
 
 
 function Ball(x, y, radius, ballColor) {
